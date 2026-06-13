3fe1279259423d677d6e7d49381c189fc43bb18b
Muhammad Idrees <idreesbangash490@gmail.com>
2026-06-13 14:46:21 -0700

    Add Day 2 project files

---
+++ Day 2/script.js
@@ -0,0 +1,28 @@
+const check = document.getElementById('check');
+const btn = document.getElementById('btn');
+const Dbtn = document.getElementById('Dbtn');
+
+
+btn.addEventListener("click", () => {
+    const wrapper = document.createElement("div");
+
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "Enter text";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    wrapper.appendChild(input);
+    wrapper.appendChild(checkbox);
+
+    check.appendChild(wrapper);
+});
+
+Dbtn.addEventListener("click", () => {
+    const lastItem = check.lastElementChild;
+
+    if (lastItem) {
+        lastItem.remove();
+    }
+});
